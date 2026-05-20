2c720a2d653a6ff582c07afe57a5442f3990f1e5
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-05-20 17:55:42 +0545

    feat add user and register serializer

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,36 @@
+# pyrefly: ignore [missing-import]
+from rest_framework import serializers
+# pyrefly: ignore [missing-import]
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'role',
+                  'company_name', 'company_website']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords don't match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'bio', 'avatar_url',
+                  'company_name', 'company_website', 'resume_url', 'skills',
+                  'github_url', 'portfolio_url']
+        read_only_fields = ['id']
