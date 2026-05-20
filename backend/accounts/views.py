45503981a141b4b4acb11b96b02a32787539d69c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-05-20 17:55:15 +0545

    feat add register view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# pyrefly: ignore [missing-import]
+from rest_framework import generics, permissions
+# pyrefly: ignore [missing-import]
+from rest_framework_simplejwt.views import TokenObtainPairView
+# pyrefly: ignore [missing-import]
+from .serializers import RegisterSerializer, UserSerializer
+# pyrefly: ignore [missing-import]
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+       queryset = User.objects.all()
+       serializer_class = RegisterSerializer
+       permission_classes = [permissions.AllowAny]
+
+class MeView(generics.RetrieveUpdateAPIView):
+       serializer_class = UserSerializer
+       permission_classes = [permissions.IsAuthenticated]
+
+       def get_object(self):
+           return self.request.user
