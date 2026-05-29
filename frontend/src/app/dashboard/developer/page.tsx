979fd67757cdf45f7139235308eb1e31060fb6f2
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-05-29 15:09:50 +0545

    feat add developer dashboard page

---
+++ frontend/src/app/dashboard/developer/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { DeveloperDashboard } from '@/components/dashboard/DeveloperDashboard';
+
+export default function DeveloperDashboardPage() {
+  return <DeveloperDashboard />;
+}
